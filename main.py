901c50055978bb0a2308ad466076937b590a85e0
huuhieupsa <huuhieupsa0903@gmail.com>
2024-10-28 09:03:06 -0700

    main

--- main.py
+++ main.py
@@ -21,8 +21,8 @@ def predict():
     
 
 # main driver function
-if __name__ == '__main__':
-    app.run()
-
 # if __name__ == '__main__':
-#     app.run(host='0.0.0.0', port=5000, debug=True)
+#     app.run()
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
